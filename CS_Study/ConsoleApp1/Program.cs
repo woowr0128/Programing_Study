89bd92e8b25aef1c57e617f953a629648d725c58
woowr0128 <woowr0128@gmail.com>
2022-11-07 23:19:05 +0900

    Study_OneDay
    VS_Text

---
+++ CS_Study/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class CS_Study
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
