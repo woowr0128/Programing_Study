e3914f2c045893b71e00c94c39171a5d2074988f
woowr0128 <woowr0128@gmail.com>
2022-11-08 20:25:20 +0900

    C# 데이터 관련 정리

--- CS_Study/ConsoleApp1/Program.cs
+++ CS_Study/ConsoleApp1/Program.cs
@@ -6,8 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //데이터 + 로직
+            // 체력 0
+            //byte(1바이트 0~255), short(2바이트~ -3만~3만), int (4바이트 -21억~21억), long(8바이트)
+            //sbyte(1바이트 -128~127), ushort(2바이트 0~6만), uint(4바이트 0~43억), ulong(8바이트)
+            int hp;
+            short level = 100;  // 레벨같은 한정적인 부분은 short등을 쓰지만 90%는 int사용. 최적화용
+            long Id; // 아이템 아이디같은 정보는 long으로 관리해야 나중가서 편함.
+
+
             Console.WriteLine("Hello World!");
-            //소스트리 커밋 확인용 주석 추가
+           
         }
     }
 }
