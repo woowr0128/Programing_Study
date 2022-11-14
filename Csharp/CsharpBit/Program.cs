37facc167664fc3556a4353b924d33c43c0ab0de
woowr0128 <woowr0128@gmail.com>
2022-11-14 23:00:01 +0900

    비트 연산

---
+++ Csharp/CsharpBit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CsharpBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 123;
+            int key = 401;
+
+            int a = id ^ key;
+            int b = a ^ key;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            // 비트 연산
+        
+        }
+    }
+}
