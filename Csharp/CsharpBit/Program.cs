2d5554d210391e2d90c7183c682acc688df27f3c
woowr0128 <woowr0128@gmail.com>
2022-11-15 22:43:39 +0900

    if와 else 조건문

--- Csharp/CsharpBit/Program.cs
+++ Csharp/CsharpBit/Program.cs
@@ -6,16 +6,36 @@ namespace CsharpBit
     {
         static void Main(string[] args)
         {
-            int id = 123;
-            int key = 401;
+            //int id = 123;
+            //int key = 401;
 
-            int a = id ^ key;
-            int b = a ^ key;
+            //int a = id ^ key;
+            //int b = a ^ key;
 
-            Console.WriteLine(a);
-            Console.WriteLine(b);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
 
             // 비트 연산
+            //--------------------------------------------------------
+
+            int choice = 0;
+
+            if (choice == 0)
+                Console.WriteLine("가위입니다");
+            else
+            {
+                if (choice == 1)
+                    Console.WriteLine("바위입니다");
+                else
+                {
+                    if (choice == 2)
+                        Console.WriteLine("보입니다.");
+                    else
+                    {
+                        Console.WriteLine("치트키입니다");
+                    }
+                }
+            }
         
         }
     }
