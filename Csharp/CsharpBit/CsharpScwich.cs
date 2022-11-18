ae50c585aa63fa4781a1d830272f87cd42dbc01b
ParkWonJae <woowr0128@gmail.com>
2022-11-18 17:52:57 +0900

    조건문 : 가위바위보 게임제작 => 가위바위보 유저의 선택과 AI의 선택 부분 제작

--- Csharp/CsharpBit/CsharpScwich.cs
+++ Csharp/CsharpBit/CsharpScwich.cs
@@ -6,29 +6,38 @@ namespace CsharpBit
     {
         static void Main(string[] args)
         {
-            int choice = 0;
-            switch(choice)
+            Random rand = new Random();
+            int aiChoice = rand.Next(0, 3); // 0~2 사이의 랜덤 값
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
             {
                 case 0:
-                    Console.WriteLine("가위입니다.");
+                    Console.WriteLine(" 당신의 선택은 가위입니다.");
                     break;
                 case 1:
-                    Console.WriteLine("보입니다.");
+                    Console.WriteLine(" 당신의 선택은 바위입니다 ");
                     break;
                 case 2:
-                    Console.WriteLine("바위입니다.");
-                    break;
-                case 3:
-                    Console.WriteLine("치트입니다");
+                    Console.WriteLine(" 당신의 선택은 보입니다");
                     break;
             }
-            // 조건문 관련 주석 추가.
-            // 가위바위보 게임 제작.
 
-            // 보는 주먹을 이긴다.
-            // 주먹은 가위를 이긴다.
-            // 가위는 보를 이긴다.
-            // A,B중에 승자는 누구인가?
+            switch (aiChoice)
+            {
+                case 0:
+                    Console.WriteLine(" 컴퓨터의 선택은 가위입니다.");
+                    break;
+                case 1:
+                    Console.WriteLine(" 컴퓨터의 선택은 바위입니다 ");
+                    break;
+                case 2:
+                    Console.WriteLine(" 컴퓨터의 선택은 보입니다");
+                    break;
+            }
+
+            //승리 무승부 패배
         }
     }
 }
