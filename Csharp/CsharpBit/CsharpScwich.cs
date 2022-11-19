c36923279795b126fbac22843699af52c98e7762
woowr0128 <woowr0128@gmail.com>
2022-11-19 15:01:49 +0900

    조건문 가위바위보 게임 완성

--- Csharp/CsharpBit/CsharpScwich.cs
+++ Csharp/CsharpBit/CsharpScwich.cs
@@ -6,6 +6,7 @@ namespace CsharpBit
     {
         static void Main(string[] args)
         {
+            // 0 : 가위, 1 : 바위 , 2 : 보
             Random rand = new Random();
             int aiChoice = rand.Next(0, 3); // 0~2 사이의 랜덤 값
 
@@ -17,6 +18,7 @@ namespace CsharpBit
                     Console.WriteLine(" 당신의 선택은 가위입니다.");
                     break;
                 case 1:
+
                     Console.WriteLine(" 당신의 선택은 바위입니다 ");
                     break;
                 case 2:
@@ -37,7 +39,51 @@ namespace CsharpBit
                     break;
             }
 
-            //승리 무승부 패배
+            if (choice == 0) // 사용자의 패가 가위일때.
+            {
+                if(aiChoice == 0) // AI가 가위라면
+                {
+                    Console.WriteLine("무승부입니다");
+                }
+                else if (aiChoice == 1) // AI 가 바위라면
+                {
+                    Console.WriteLine(" 패배입니다.");
+                }
+                else if (aiChoice == 2) // AI 가 보라면
+                {
+                    Console.WriteLine(" 승리입니다.");
+                }
+            }
+            if (choice == 1) // 사용자의 패가 바위일때.
+            {
+                if (aiChoice == 0) 
+                {
+                    Console.WriteLine(" 승리입니다. ");
+                }
+                else if (aiChoice == 1) 
+                {
+                    Console.WriteLine(" 무승부입니다.");
+                }
+                else if (aiChoice == 2) 
+                {
+                    Console.WriteLine(" 패배입니다.");
+                }
+            }
+            if (choice == 2) // 사용자의 패가 보일때.
+            {
+                if (aiChoice == 0) 
+                {
+                    Console.WriteLine(" 패배입니다. ");
+                }
+                else if (aiChoice == 1) 
+                {
+                    Console.WriteLine(" 승리입니다.");
+                }
+                else if (aiChoice == 2) 
+                {
+                    Console.WriteLine(" 무승부입니다.");
+                }
+            }
         }
     }
 }
