7c4286593c4b5b4992dfe1c88c7a347d3a342b42
ParkWonJae <woowr0128@gmail.com>
2022-11-21 17:50:28 +0900

    while 문

---
+++ Csharp/CsharpBit/CsharpWhile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CsharpBit
+{
+    class CsharpWhile
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            do
+            {
+                Console.WriteLine("강사님은 잘생기셨나요? (y/n)");
+                answer = Console.ReadLine();
+
+            } while (answer != "y");
+
+            Console.WriteLine("정답입니다(?)");
+        }
+    }
+}
