5234e82f7d41a0c2788c22b1bb72975ea60a4bdf
ParkWonJae <woowr0128@gmail.com>
2022-11-22 14:34:51 +0900

    C# 조건문 for

---
+++ Csharp/CsharpBit/Csharpfor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CsharpBit
+{
+    class Csharpfor
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while (count < 5)
+            {
+                Console.WriteLine("Hellow World");
+            }
+            //for (초기화식; 조건식; 반복식)
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hellow World");
+            }
+        }
+    }
+}
