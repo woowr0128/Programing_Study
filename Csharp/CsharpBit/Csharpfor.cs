2e1f2d7f2988fa8a7d4dfdf955cd1a8a04375ded
ParkWonJae <woowr0128@gmail.com>
2022-11-25 17:24:19 +0900

    break, continue 추가 예시

--- Csharp/CsharpBit/Csharpfor.cs
+++ Csharp/CsharpBit/Csharpfor.cs
@@ -37,6 +37,14 @@ namespace CsharpBit
             {
                 Console.WriteLine("소수가 아닙니다.");
             }
+
+            for (int i=1; i <= 100; i++)
+            {
+                if((i % 3) == 0)
+                {
+                    Console.WriteLine($"3으로 나뉘는 숫자 발견 : {i}");
+                }
+            }
         }
     }
 }
