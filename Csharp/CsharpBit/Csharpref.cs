93b8d035af119711c71be9e0a65b8debea204a7f
ParkWonJae <woowr0128@gmail.com>
2022-12-01 17:44:48 +0900

    ref, out

---
+++ Csharp/CsharpBit/Csharpref.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CsharpBit
+{
+    class Csharpref
+    {
+        static void Divide(int a, int b, out int result1, out int result2)
+        {
+            result1 = a / b;
+            result2 = a % b;
+        }
+
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 3;
+
+            int result1;
+            int result2;
+
+            Divide(10, 3, out result1, out result2);
+
+            Console.WriteLine(result1);
+        }
+    }
+}
