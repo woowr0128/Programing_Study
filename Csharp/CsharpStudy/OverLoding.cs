fa42cf600fd70bc7bb8e6224ec07e26a11e5fbb0
ParkWonJae <woowr0128@gmail.com>
2022-12-06 16:49:01 +0900

    OverLoding

---
+++ Csharp/CsharpStudy/OverLoding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CsharpStudy
+{
+    class OverLoding
+    {
+        //함수 이름의 재사용
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static float Add(float a, float b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            int ret = OverLoding.Add(2, 3);
+            float ret2 = OverLoding.Add(2.0f, 3.0f);
+                
+        }
+    }
+}
