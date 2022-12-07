aa526a163fee0983b6414219901089365c69a64f
ParkWonJae <woowr0128@gmail.com>
2022-12-07 17:12:00 +0900

    연습예제1(구구단 연속 출력시키기)

---
+++ Csharp/CsharpStudy/PracticeProblem1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CsharpStudy
+{
+    class PracticeProblem1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                
+            }
+        }
+    }
+}
