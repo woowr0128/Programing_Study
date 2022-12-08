a3283131108173de1210b043ac8e23bf60b35535
ParkWonJae <woowr0128@gmail.com>
2022-12-08 17:05:31 +0900

    연습예제2

--- Csharp/CsharpStudy/PracticeProblem1.cs
+++ Csharp/CsharpStudy/PracticeProblem1.cs
@@ -6,14 +6,23 @@ namespace CsharpStudy
     {
         static void Main(string[] args)
         {
-            for (int i = 2; i <= 9; i++)
-            {
-                for (int j = 1; j <= 9; j++)
-                {
-                    Console.WriteLine($"{i} * {j} = {i * j}");
-                }
+            //for (int i = 2; i <= 9; i++)
+            //{
+            //    for (int j = 1; j <= 9; j++)
+            //    {
+            //        Console.WriteLine($"{i} * {j} = {i * j}");
+            //    }
                 
+            //}
+            for (int i = 0; i<5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine();
             }
+
         }
     }
 }
