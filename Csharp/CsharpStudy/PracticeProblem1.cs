f9a2060fcb4dfbb9582b7153c36c07590fbca720
ParkWonJae <woowr0128@gmail.com>
2022-12-14 16:03:05 +0900

    예제 정리

--- Csharp/CsharpStudy/PracticeProblem1.cs
+++ Csharp/CsharpStudy/PracticeProblem1.cs
@@ -5,7 +5,9 @@ namespace CsharpStudy
     class PracticeProblem1
     {
         static void Main(string[] args)
-        {
+        {   
+            //예제1
+            //----------------------------------------------------
             //for (int i = 2; i <= 9; i++)
             //{
             //    for (int j = 1; j <= 9; j++)
@@ -14,14 +16,18 @@ namespace CsharpStudy
             //    }
                 
             //}
-            for (int i = 0; i<5; i++)
-            {
-                for (int j = 0; j <= i; j++)
-                {
-                    Console.WriteLine("*");
-                }
-                Console.WriteLine();
-            }
+            //-----------------------------------------------------
+            //예제2
+            //-----------------------------------------------------
+            //for (int i = 0; i<5; i++)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.WriteLine("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //------------------------------------------------------
 
             //2022-12-09 예제 3번 추가예정
             // 주석 추가 테스트
