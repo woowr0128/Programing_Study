c6b6ec64d6776f85bfdbe1efd64fc1141a31db4d
ParkWonJae <woowr0128@gmail.com>
2022-12-15 17:15:17 +0900

    Debug

--- Csharp/CsharpStudy/Program.cs
+++ Csharp/CsharpStudy/Program.cs
@@ -4,22 +4,15 @@ namespace CsharpStudy
 {
     class Program
     {
+        static int Add(int a, int b)
+        {
+            int ret = a + b;
+            return ret;
+        }
         static void Main(string[] args)
         {
-            int hp = 100;
-            int level = 50;
-
-            bool isAlive = (hp > 0);
-            bool isHighLevel = (level >= 40);
-
-            // && AND  || OR ! NOT
-            // a = 살아있는 고렙 유저인가요?
-            bool a = isAlive && isHighLevel;
-
-            // b = 살아있거나, 고렙 유저이거나, 둘 중 하나 인가요?
-            bool b = isAlive || isHighLevel;
-            // c =  죽은 유저인가요?
-            bool c = !isAlive; 
+            int ret = Program.Add(10, 20);
+            Console.WriteLine(ret);
         }
     }
 }
